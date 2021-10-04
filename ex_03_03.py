d9e6662c8b03c1045760f61e91c2b680bfd2dd5b
Daniel <42197094+Daniel6628@users.noreply.github.com>
2021-10-04 00:18:18 -0400

    Create ex_03_03.py

---
+++ ex_03_03.py
@@ -0,0 +1,19 @@
+score = input("Enter Score: ")
+try:
+    score = float(score)
+except:
+    print('Error')
+    quit()
+if 0.0 <= score <= 1.0:
+    if score >= 0.9:
+        print('A')
+    elif score >= 0.8:
+        print('B')
+    elif score >= 0.7:
+        print('C')
+    elif score >= 0.6:
+        print('D')
+    else:
+        print('F')
+else:
+    print('Error: Not in the range')
