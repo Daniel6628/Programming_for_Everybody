f0164866b293dee7d247dc30f378dff2814fc6ca
Daniel <42197094+Daniel6628@users.noreply.github.com>
2021-10-09 23:28:46 -0400

    Update ex_11_01.py

--- ex_11_01.py
+++ ex_11_01.py
@@ -5,8 +5,9 @@ Handling The Data
 The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' 
 and then converting the extracted strings to integers and summing up the integers.
 
-The file can be found [here](http://py4e-data.dr-chuck.net/regex_sum_1377118.txt)
 '''
+The file can be found [here](http://py4e-data.dr-chuck.net/regex_sum_1377118.txt)
+
 
 
 import re
