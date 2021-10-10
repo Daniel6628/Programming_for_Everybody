cd132a37a63f9e42b9bc55f5bea272a8d9d261fb
Daniel <42197094+Daniel6628@users.noreply.github.com>
2021-10-09 23:31:41 -0400

    Update ex_11_01.py

--- ex_11_01.py
+++ ex_11_01.py
@@ -5,9 +5,8 @@ Handling The Data
 The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' 
 and then converting the extracted strings to integers and summing up the integers.
 
+The file can be found here: http://py4e-data.dr-chuck.net/regex_sum_1377118.txt
 '''
-The file can be found [here](http://py4e-data.dr-chuck.net/regex_sum_1377118.txt)
-
 
 
 import re
@@ -18,6 +17,7 @@ count = 0
 for line in hand:
     line = line.rstrip()
     stuff = re.findall('[0-9]+', line)
+    #use a list comprehension
     num = [int(i) for i in stuff]
     nums = sum(num)
     lst.append(nums)
